b9c8342e8ab4228406e416258abf60d0815a3caa
Andrew <theandrewkimm@gmail.com>
2017-05-25 16:33:17 -0700

    finish modern roman recursively

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,3 +1,31 @@
-def convert_to_roman(arabic_number)
-  # Your code goes here
+def numerals
+  romans =
+  {
+    1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+    90 => "XC",
+    50 => "L",
+    40 => "XL",
+    10 => "X",
+    9 => "IX",
+    5 => "V",
+    4 => "IV",
+    1 => "I"
+  }
+end
+
+def convert_to_roman(arabic_number, numeral="")
+  while arabic_number >= 1
+    romans.each do |key, value|
+      if arabic_number >= key
+        numeral += value
+        arabic_number -= key
+        convert_to_roman(arabic_number, numeral)
+      end
+    end
+  end
+  return numeral
 end
