56cd1adf7c3d54eb2a883fc7da7bc87784b5c6b6
lilcupcakey <yoona16@gmail.com>
2017-05-26 04:17:32 +0900

    commit message

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,7 +1,17 @@
 def convert_to_roman(arabic_number)
-  if arabic_number == 1
-    arabic_number = 'I'
 
-  if arabic_number == 4
-      arabic_number = 'IIII'
+  xWhole = arabic_number/10
+  vRemain = arabic_number%10
+
+  vWhole = vRemain/5
+  iRemain = arabic_number%5
+
+  xTotal = "X"*xWhole
+  vTotal = "V"*vWhole
+  iTotal = "I"*iRemain
+
+  arabic_number = xTotal + vTotal + iTotal
+
 end
+
+puts convert_to_roman(31)
